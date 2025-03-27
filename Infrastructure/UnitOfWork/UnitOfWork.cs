3f7feccb0a16b9507821f9a3ccccc3efa19553b6
Marek Marcinko <Marek.Marcinko@accace.com>
2025-03-27 08:53:16 +0100

    UnitOfWork fix

--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,11 +1,17 @@
 ﻿namespace Infrastructure.UnitOfWork;
 
-public class UnitOfWork(ApplicationDbContext context) : IUnitOfWork
+public class UnitOfWork(ApplicationDbContext context) : IUnitOfWork, IDisposable
 {
     private readonly ApplicationDbContext context = context;
 
     public async Task CompleteAsync(CancellationToken cancellationToken = default)
     {
         await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        context.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
