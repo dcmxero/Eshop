21ce36cfceb91eb6d3fa5e67ff12a167cd9930ad
Marek Marcinko <marek.marcinko@ngaviation.eu>
2025-11-04 21:26:53 +0100

    Remove unnecessary lines for primary constuctor + update gitIgnore

--- Application/Features/Products/GetActiveProductsHandler.cs
+++ Application/Features/Products/GetActiveProductsHandler.cs
@@ -17,8 +17,6 @@ namespace Application.Features.Products;
 /// <param name="productService">The product service used to fetch active products.</param>
 public class GetActiveProductsHandler(IProductService productService) : IRequestHandler<GetActiveProductsRequest, PaginatedList<ProductDto>>
 {
-    private readonly IProductService productService = productService;
-
     /// <summary>
     /// Handles the GetActiveProductsRequest and returns a paginated list of active products.
     /// </summary>
